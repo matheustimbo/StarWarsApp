2a6aae3679a68aec449f01962adeeec12bd64d07
Matheus Timbó <matheusfild4@hotmail.com>
2020-11-19 10:00:51 -0300

    Adding infinite scroll

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity, Text, FlatList} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import usePeople from '../../hooks/usePeople';
 import {People} from '../../providers/starWarsContext';
 import PeopleItem from './components/PeopleItem';
+import ListLoadingIndicator from './components/ListLoadingIndicator';
+
+const {width, height} = Dimensions.get('window');
 
 const Home: React.FC = () => {
   const {people, loadingPeople, fetchPeoples} = usePeople();
+
   return (
     <View style={styles.container}>
       <FlatList
         data={people}
+        style={styles.list}
+        onEndReached={() => fetchPeoples()}
+        onEndReachedThreshold={0.5}
+        keyExtractor={({index}) => index}
         renderItem={({item}) => <PeopleItem people={item} />}
+        ListFooterComponent={() => (
+          <ListLoadingIndicator loadingPeople={loadingPeople} />
+        )}
       />
     </View>
   );
@@ -19,6 +37,17 @@ const Home: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: height - 100,
+  },
+  list: {
+    width,
+    height: height - 100,
+  },
+  loadingIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 12,
   },
 });
 
